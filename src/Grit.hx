66eb5f2b40b652fbbb25813e302ac4626826627c
Justin Donaldson <jdonaldson@gmail.com>
2018-09-18 14:08:53 -0700

    multiple grits per tag

--- src/Grit.hx
+++ src/Grit.hx
@@ -1,20 +1,18 @@
 import sys.io.Process;
+using StringTools;
 
 class Grit {
 
     static function shell(cmd : String) : String {
-        var args = ~/\s+/.split(cmd);
-        var p = new Process('sh', args);
+        var p = new Process(cmd);
         var exit = p.exitCode(true);
         if (exit != 0){
             throw ('Error : ' + p.stderr.readAll().toString());
         }
-        return p.stdout.readAll().toString();
+        return p.stdout.readAll().toString().trim();
     }
     static function git(cmdstr : String) : String {
-        var args = ~/\s+/g.split(cmdstr);
-        var cmd = args.shift();
-        var p = new Process('git $cmd', args);
+        var p = new Process('git $cmdstr');
         var exit = p.exitCode(true);
         if (exit != 0){
             var err = p.stderr.readAll().toString();
@@ -25,11 +23,9 @@ class Grit {
         return p.stdout.readAll().toString();
     }
 
-    public static function commit(branch : String, payload : String) : Void {
+    public static function commit(branch : String) : Void {
         git("add .");
-        git('commit -m $branch --quiet');
-        var payload = ~/"/.replace(payload, "\\\"");
-        git('tag -a grit-$branch -m $payload');
+        git('commit -m grit-$branch --quiet');
     }
 
     public static function isDirty() : Bool {
@@ -38,13 +34,28 @@ class Grit {
         var index = status.lastIndexOf(message);
         return index == -1;
     }
+    public static function deleteGritTags(){
+        shell("git tag -l grit-* | xargs git tag -d");
+    }
 
     public static function log(metric : String, value : Float){
         var branch = git("rev-parse HEAD").substr(0,8);
-        var payload = {metric : metric, value : value};
+        var payload = {branch : branch, metric : metric, value : value};
         var payload_str = haxe.Json.stringify(payload);
+        payload_str = ~/"/g.replace(payload_str, "\\\"");
+
         if (isDirty()){
-            commit(branch, payload_str);
+            commit(branch);
+        }
+        if (git('tag -l grit-$branch') == ""){
+            git('tag -a grit-$branch -m "$payload_str"');
+        } else {
+            var current = shell('git show grit-$branch --quiet --format=%b | tail -n +4');
+            current = ~/"/g.replace(current, "\\\"");
+            if (current.indexOf(payload_str) == -1){
+                current += '\n$payload_str';
+                shell('git tag -fa grit-$branch -m "$current"');
+            }
         }
 
 
--- test/Main.hx
+++ test/Main.hx
@@ -1,7 +1,10 @@
 import Grit;
 class Main {
     static function main() {
+        Grit.deleteGritTags();
         Grit.log("foo", 2);
-        Grit.log("bar", 4);
+        Grit.log("bar", 2);
+        Grit.log("baz", 2);
+        Grit.log("bing", 2);
     }
 }
