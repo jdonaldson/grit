53e27865dcd42a33ab776b323d571e015d761f9e
Justin Donaldson <jdonaldson@gmail.com>
2018-09-18 12:27:09 -0700

    ba6481a3 --quiet

--- src/Grit.hx
+++ src/Grit.hx
@@ -1,35 +1,52 @@
 import sys.io.Process;
-import com.hurlant.crypto.prng.Random;
 
 class Grit {
 
-    static function git(cmd : String) : String {
+    static function shell(cmd : String) : String {
         var args = ~/\s+/.split(cmd);
-        var cmd = args.shift();
-        var p = new Process('git $cmd', args);
+        var p = new Process('sh', args);
         var exit = p.exitCode(true);
         if (exit != 0){
             throw ('Error : ' + p.stderr.readAll().toString());
         }
         return p.stdout.readAll().toString();
     }
+    static function git(cmdstr : String) : String {
+        var args = ~/\s+/g.split(cmdstr);
+        var cmd = args.shift();
+        var p = new Process('git $cmd', args);
+        var exit = p.exitCode(true);
+        if (exit != 0){
+            var err = p.stderr.readAll().toString();
+            var out = p.stdout.readAll().toString();
+            var msg = err.length > 0 ? err : out;
+            throw ('Error : $cmdstr\n Message: $msg');
+        }
+        return p.stdout.readAll().toString();
+    }
 
-    public static function commit(branch : String){
+    public static function commit(branch : String, payload : String) : Void {
         git("add .");
         git('commit -m $branch --quiet');
-        git('tag -l "grit-$branch"');
+        var payload = ~/"/.replace(payload, "\\\"");
+        git('tag -a grit-$branch -m $payload');
+    }
+
+    public static function isDirty() : Bool {
+        var message = "nothing to commit, working tree clean";
+        var status = git('status');
+        var index = status.lastIndexOf(message);
+        return index == -1;
     }
 
     public static function log(metric : String, value : Float){
-        var random = new Random();
-
         var branch = git("rev-parse HEAD").substr(0,8);
-        trace(branch + " is the value for branch");
-        var fout = sys.io.File.append("grit.log");
         var payload = {metric : metric, value : value};
         var payload_str = haxe.Json.stringify(payload);
-        fout.writeString('grit-$branch $payload_str');
-        fout.close();
+        if (isDirty()){
+            commit(branch, payload_str);
+        }
+
 
     }
 
--- test/Main.hx
+++ test/Main.hx
@@ -2,5 +2,6 @@ import Grit;
 class Main {
     static function main() {
         Grit.log("foo", 2);
+        Grit.log("bar", 4);
     }
 }
