ba6481a3c8a75365297d458f2f91336a0e88ea95
Justin Donaldson <jdonaldson@gmail.com>
2018-09-18 12:20:15 -0700

    use tags

--- src/Grit.hx
+++ src/Grit.hx
@@ -1,60 +1,36 @@
 import sys.io.Process;
-import com.hurlant.crypto.extra.UUID;
 import com.hurlant.crypto.prng.Random;
 
 class Grit {
 
-    static function shell(cmd : String) : String {
+    static function git(cmd : String) : String {
         var args = ~/\s+/.split(cmd);
         var cmd = args.shift();
-        var p = new Process(cmd, args);
+        var p = new Process('git $cmd', args);
         var exit = p.exitCode(true);
         if (exit != 0){
             throw ('Error : ' + p.stderr.readAll().toString());
         }
         return p.stdout.readAll().toString();
     }
-    static function git(args : String){
-        return shell('git $args --git-dir=.grit');
-    }
 
     public static function commit(branch : String){
         git("add .");
         git('commit -m $branch --quiet');
-        git('checkout master --quiet');
-        git('merge $branch --quiet');
-        git('branch -d $branch --quiet');
-    }
-    static function checkStatus() {
-        return false;
+        git('tag -l "grit-$branch"');
     }
 
     public static function log(metric : String, value : Float){
         var random = new Random();
 
-        var branch = UUID.generateRandom(random).toString().substr(0,8);
+        var branch = git("rev-parse HEAD").substr(0,8);
         trace(branch + " is the value for branch");
         var fout = sys.io.File.append("grit.log");
+        var payload = {metric : metric, value : value};
+        var payload_str = haxe.Json.stringify(payload);
+        fout.writeString('grit-$branch $payload_str');
+        fout.close();
 
-        if (!sys.FileSystem.isDirectory(".grit")){
-            init();
-            var metric = haxe.Json.stringify({metric : metric, value : value});
-            fout.writeString('$branch $metric\n');
-            fout.close();
-        } else if (!checkStatus()) {
-            trace("HI");
-
-
-        }
-
-
-
-    }
-    public static function init() {
-        shell("echo '.grit' >> .gitignore");
-        shell("git init --separate-git-dir .grit");
-        git("add .");
-        git("commit -m 'grit-head'");
     }
 
 }
